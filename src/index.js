c7b1bf811b041e9286971065f73110c469166626
Levent DEMIR <l.demir@qwant.com>
2018-03-23 15:26:18 +0100

    first version

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@
     POI_3: 'Cafeteria'
   }
 
+  console.log(window)
+  var STORE = 'https://sync-beta.qwantresearch.com/'
+  var masqStore = new MasqClient(STORE)
+
   var ENTER_KEY = 13
   var newTodoDom = document.getElementById('new-todo')
 
@@ -145,8 +149,8 @@
     console.log('0.2', todos)
     if (todos) {
       todos.forEach(function (todo) {
-      // console.log("0.3", todo)
-      // ul.appendChild(createTodoListItem(todo))
+        // console.log("0.3", todo)
+        // ul.appendChild(createTodoListItem(todo))
         ul.appendChild(createTodoListItem(todo))
       })
     }
@@ -164,25 +168,6 @@
     newTodoDom.addEventListener('keypress', newTodoKeyPressHandler, false)
   }
 
-  function handshake () {
-    window.postMessage(
-      {
-        from: 'webpage',
-        to: 'background',
-        type: 'handshake',
-        data: 'hello'
-      }, '*')
-  }
-  function initDB () {
-    window.postMessage(
-      {
-        from: 'webpage',
-        to: 'background',
-        type: 'initDB',
-        data: 'QwantNote'
-      }, '*')
-  }
-
   const delay = (ms) => {
     console.log('wait ...')
     return new Promise(function (resolve, reject) {
@@ -191,24 +176,73 @@
   }
 
   const setItem = (key, value) => {
-    window.postMessage(
-      {
-        from: 'webpage',
-        to: 'background',
-        type: 'set',
-        data: JSON.stringify({ key: key, value: value })
-      }, '*')
+    console.log('receive', value)
+    // masqStore.get(key).then(function (actualNotes) {
+    //   console.log('actual', actualNotes)
+    //   actualNotes.push(value)
+    //   console.log('new', actualNotes)
+    masqStore.set(key, value)
+      .then(() => {
+        // success
+        console.log('done')
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  // var STORE = 'https://sync-beta.qwantresearch.com/'
+  var appURL = window.location.origin + window.location.pathname
+  var regParams = {
+    endpoint: STORE,
+    url: appURL,
+    title: 'Masq Todo app',
+    desc: 'A generic app that uses Masq for storage',
+    icon: 'https://camo.githubusercontent.com/8b35d12bd9682a31446b08c1483145653aa5006f/68747470733a2f2f692e696d6775722e636f6d2f715a33647130512e706e67'
   }
 
   const getItem = (key) => {
-    window.postMessage(
-      {
-        from: 'webpage',
-        to: 'background',
-        type: 'get',
-        data: JSON.stringify({ key: key })
-      }, '*')
+    return masqStore.get(key).then(function (res) {
+      console.log(res) // prints "Hello world"
+      if (res) {
+        return res
+      } else { return [] }
+    }).catch(function (err) {
+      if (err.message === 'UNREGISTERED') {
+        masqStore.registerApp(regParams).then(function (e) {
+          // window.location.reload()
+          getItem(key)
+        })
+      }
+      console.log(err.message)
+    })
   }
+
+  const getAll = (key) => {
+    return masqStore.getAll(key).then(function (res) {
+      console.log(res) // prints "Hello world"
+      if (res) {
+        return res
+      } else { return [] }
+    }).catch(function (err) {
+      if (err.message === 'UNREGISTERED') {
+        masqStore.registerApp(regParams).then(function (e) {
+          // window.location.reload()
+          getItem(key)
+        })
+      }
+      console.log(err.message)
+    })
+  }
+
+  masqStore.onConnect().then(res => {
+    getAll('notes').then(notes => {
+      if (Object.keys(notes).length === 0 && notes.constructor === Object) { } else {
+        currentNotes = notes.notes
+        redrawTodosUI(notes.notes)
+      }
+    }).catch(err => console.log(err))
+  }).catch(err => console.log(err))
 
   addEventListeners()
   handshake()
@@ -228,52 +262,58 @@
     setItem('notes', notes)
   }
 
-  delay(1000).then(() => {
-    getItem('notes')
-  })
-
   // redrawTodosUI(notes.rows)
   // showTodos()
 
-  /**
-  *  Receive messages from contentscript
-  *  Transfer them to webpage.
-  */
-  window.addEventListener('message', function (event) {
-    // We only accept messages from ourselves
-    let parsedData = null
-    if (event.source != window) { return }
+  // /**
+  // *  Receive messages from contentscript
+  // *  Transfer them to webpage.
+  // */
+  // window.addEventListener('message', function (event) {
+  //   // We only accept messages from ourselves
+  //   let parsedData = null
+  //   if (event.source != window) { return }
 
-    if (event.data.to && (event.data.to == 'webpage')) {
-      switch (event.data.type) {
-        case 'handshake_ack':
-          console.log('Webpage script : ')
-          console.log(`${event.data.to} receives a ${event.data.type} from ${event.data.from}`)
-          //   console.log("Content script sends this message to backgound: " + event.data)
+  //   if (event.data.to && (event.data.to == 'webpage')) {
+  //     switch (event.data.type) {
+  //       case 'handshake_ack':
+  //         console.log('Webpage script : ')
+  //         console.log(`${event.data.to} receives a ${event.data.type} from ${event.data.from}`)
+  //         //   console.log("Content script sends this message to backgound: " + event.data)
 
-          break
-        case 'set_ack':
-          console.log('Webpage script : ')
-          console.log(`${event.data.to} receives a ${event.data.type} from ${event.data.from}`)
-          //   console.log("Content script sends this message to backgound: " + event.data)
+  //         break
+  //       case 'set_ack':
+  //         console.log('Webpage script : ')
+  //         console.log(`${event.data.to} receives a ${event.data.type} from ${event.data.from}`)
+  //         //   console.log("Content script sends this message to backgound: " + event.data)
 
-          break
-        case 'get_ack':
-          parsedData = JSON.parse(event.data.data)
-          console.log('Webpage script : ')
-          console.log(`${event.data.to} receives a ${event.data.type} from ${event.data.from}`)
-          console.log()
-          if (parsedData.value) {
-            currentNotes = parsedData.value
-          }
-          redrawTodosUI(currentNotes)
-          //   console..datalog("Content script sends this message to backgound: " + event.data)
+  //         break
+  //       case 'get_ack':
+  //         parsedData = JSON.parse(event.data.data)
+  //         console.log('Webpage script : ')
+  //         console.log(`${event.data.to} receives a ${event.data.type} from ${event.data.from}`)
+  //         console.log()
+  //         if (parsedData.value) {
+  //           currentNotes = parsedData.value
+  //         }
+  //         redrawTodosUI(currentNotes)
+  //         //   console..datalog("Content script sends this message to backgound: " + event.data)
 
-          break
+  //         break
 
-        default:
-          break
-      }
+  //       default:
+  //         break
+  //     }
+  //   }
+  // }, false)
+
+  // Refresh app state following a sync event
+  document.addEventListener('Sync', function (event) {
+    console.log('Sync event!')
+    if (masqStore) {
+      getAll('notes').then(notes => {
+        if (Object.keys(notes).length === 0 && notes.constructor === Object) { } else { redrawTodosUI(notes.notes) }
+      }).catch(err => console.log(err))
     }
-  }, false)
+  })
 })()
